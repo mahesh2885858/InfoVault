84bd5a6ecef48f25053e8fdc56cb3298cdd665ef
mahesh <mahesh2885858@gmail.com>
2024-07-01 22:35:52 +0530

    fix bugs

--- ios/InfoVaultTests/InfoVaultTests.m
+++ ios/InfoVaultTests/InfoVaultTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface infoValutTests : XCTestCase
+@interface InfoVaultTests : XCTestCase
 
 @end
 
-@implementation infoValutTests
+@implementation InfoVaultTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
