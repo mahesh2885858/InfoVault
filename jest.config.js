53b4608708bfee20aad2b3ce68a336e8ae6ed637
mahesh <mahesh2885858@gmail.com>
2024-07-10 22:12:19 +0530

    add init test files

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+
+  // testPathIgnorePatterns: [
+  //   'node_modules/(?!(@react-native|react-native|react-native-vector-icons)/)',
+  // ],
 };
