f49c7638554c0ed1c7b7c5e99cdd1a750307c064
mahesh2885858 <mahesh2885858@gmail.com>
2024-11-18 23:41:55 +0530

    feat: Add react-native-scoped-storage for data export functionality

--- src/Screens/Settings/index.tsx
+++ src/Screens/Settings/index.tsx
@@ -4,11 +4,36 @@ import Container from '../../components/atoms/Container';
 import LightText from '../../components/atoms/LightText';
 import PressableWithFeedback from '../../components/PressableWithFeedback';
 import RNFS from 'react-native-fs';
+import * as ScopedStorage from 'react-native-scoped-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 const Settings = () => {
   const exportData = async () => {
     try {
-      const path = RNFS.DocumentDirectoryPath + '/data.json';
-      await RNFS.writeFile(path, JSON.stringify({name: 'mahesh'}), 'utf8');
+      let dir = await AsyncStorage.getItem('exportPath');
+      if (!dir) {
+        dir = (await ScopedStorage.openDocumentTree(true)).uri;
+        AsyncStorage.setItem('exportPath', dir);
+      } else {
+        dir = JSON.parse(dir);
+      }
+      if (!dir) return;
+
+      const persistedUris = await ScopedStorage.getPersistedUriPermissions();
+
+      if (persistedUris.indexOf(dir) !== -1) {
+        // If uri is found, we can proceed to write/read data.
+        const data = await ScopedStorage.writeFile(
+          dir,
+          JSON.stringify({data: 'data'}),
+          'data.json',
+          'application/json',
+          'utf8',
+        );
+      } else {
+        // We can request for permission again and store the new directory if access has been revoked by user here.
+        dir = (await ScopedStorage.openDocumentTree(true)).uri;
+      }
     } catch (e) {
       console.log(e);
     }
