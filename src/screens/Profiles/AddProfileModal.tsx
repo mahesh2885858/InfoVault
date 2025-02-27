52dbfe1fac3ca9e73bf6f934a8abd7aab442aa7b
mahesh2885858 <mahesh2885858@gmail.com>
2025-02-27 18:13:50 +0530

    add modal to add profile

--- src/screens/Profiles/AddProfileModal.tsx
+++ src/screens/Profiles/AddProfileModal.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ModalWrapper from '../../components/ModalWrapper';
 import Container from '../../components/atoms/Container';
 import {TextInput} from 'react-native';
+import Button from '../../components/atoms/Button';
+import {StyleService} from '@ui-kitten/components';
+import {StyleSheet} from 'react-native';
+import {View} from 'react-native';
+import PressableWithFeedback from '../../components/PressableWithFeedback';
+import LightText from '../../components/atoms/LightText';
 
 type TProps = {
   onClose: () => void;
@@ -11,10 +17,24 @@ type TProps = {
 const AddProfileModal = (props: TProps) => {
   return (
     <ModalWrapper visible={props.visible} onClose={props.onClose}>
-      <Container>
-        <TextInput />
+      <Container
+        style={{
+          width: '100%',
+        }}>
+        <TextInput style={styles.input} />
+        <PressableWithFeedback>
+          <LightText>Add</LightText>
+        </PressableWithFeedback>
       </Container>
     </ModalWrapper>
   );
 };
 export default AddProfileModal;
+
+const styles = StyleSheet.create({
+  input: {
+    width: '80%',
+    padding: 10,
+    fontSize: 20,
+  },
+});
--- src/screens/Profiles/index.tsx
+++ src/screens/Profiles/index.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View} from 'react-native';
 import LightText from '../../components/atoms/LightText';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {StyleSheet} from 'react-native';
 import {myTheme} from '../../../theme';
 import Fab from '../../components/Fab';
+import AddProfileModal from './AddProfileModal';
 
 const Profiles = () => {
+  const [renderAddModal, setRenderAddModal] = useState(false);
   return (
     <SafeAreaView style={styles.container}>
       <LightText>Profiles</LightText>
-      <Fab callBack={() => {}} />
+      <Fab
+        callBack={() => {
+          setRenderAddModal(true);
+        }}
+      />
+      {renderAddModal && (
+        <AddProfileModal
+          onClose={() => setRenderAddModal(false)}
+          visible={renderAddModal}
+        />
+      )}
     </SafeAreaView>
   );
 };
